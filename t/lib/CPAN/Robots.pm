b452650d51de592d49c0083491121f322da45b84
Neil Bowers <neil@bowers.com>
2014-05-05 09:52:08 +0100

    Added a test for the 'path' attribute

--- t/lib/CPAN/Robots.pm
+++ t/lib/CPAN/Robots.pm
@@ -6,4 +6,18 @@ with 'MooX::Role::CachedURL';
 
 has '+url' => (default => sub { 'http://www.cpan.org/robots.txt' });
 
+sub content
+{
+    my $self    = shift;
+    my $fh      = $self->open_file;
+    my $content = '';
+    local $_;
+
+    while (<$fh>) {
+        $content .= $_;
+    }
+    $self->close_file($fh);
+    return $content;
+}
+
 1;
